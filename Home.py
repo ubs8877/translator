f878c73d2552aec4eb30e53f2b47759294c68716
OmiDev <ubs1977@gmail.com>
2024-03-16 22:28:50 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Translator",
+    page_icon="✍️",
+)
+
+st.snow()
+
+st.write("# Welcome to Translator ! ✍️")
+
+#st.sidebar.warning("Exit")
+
---
+++ pages/English 2 Hindi.py
@@ -0,0 +1,33 @@
+import streamlit as st 
+from deep_translator import GoogleTranslator
+import streamlit_shadcn_ui as ui
+
+st.set_page_config(
+    page_title="English 2 Hindi",
+    page_icon="✍️",
+)
+
+
+with st.container(border=True):
+
+    st.title('English - Hindi Translator')
+    msgs = st.text_area('Enter text to translate in Hindi unicode',key='txtContents')
+
+
+    def clear_form():
+        st.session_state["txtContents"] = ""
+
+    def translate_txt():
+        hindi_paragraph = GoogleTranslator(source='en', target='hi').translate(msgs)
+        st.markdown('<font color="blue">Your translated text is : </font>', unsafe_allow_html=True)
+        st.write(hindi_paragraph)
+    
+
+    with st.form("myform"):
+        c1,c2 = st.columns([1,1])
+        with c1:
+            submit = st.form_submit_button(label="Submit",on_click=translate_txt)
+        with c2:
+            clear = st.form_submit_button(label="Clear", on_click=clear_form)
+
+st.toast('Welcome to my first English to Marathi translator app - :blue[Umesh S ] :sunglasses:')
