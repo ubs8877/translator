17269041af882c747a951798c914564d82f6fd8f
OmiDev <ubs1977@gmail.com>
2024-03-16 22:38:43 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,7 @@
-import streamlit as st
+import streamlit as st 
+from deep_translator import GoogleTranslator
+import streamlit_shadcn_ui as ui
+
 
 st.set_page_config(
     page_title="Translator",
